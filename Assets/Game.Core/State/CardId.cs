cc53b7ce1727cb26ac0428958fe180982cb1afb8
vsevolodson <son.sev.06@gmail.com>
2026-07-27 17:07:03 +0900

    feat: добавлен CardId и его пустой конвертер

---
+++ Assets/Game.Core/State/CardId.cs
@@ -0,0 +1,24 @@
+using System;
+using Newtonsoft.Json;
+
+[JsonConverter (typeof(CardIdJsonConverter))]
+public readonly struct CardId : IEquatable<CardId>
+{
+    private readonly string _value;
+
+    public CardId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentExeption("card id must not be empty");
+
+        _value = value;
+    }
+
+    public string Value => _value ?? string.Empty;
+    public bool Equals(CardId other) => string.Equals(Value, other.Value, StringComparison.Ordinal);
+    public override bool Equals(object obj) => obj is CardId other && Equals(other);
+    public override int GetHashCode() => StringComparer.Ordinal.GetHashCode(Value);
+    public override string ToString() => Value;
+    public static bool operator ==(CardId left, CardId right) => left.Equals(right);
+    public static bool operator !=(CardId left, CardId right) => !left.Equals(right);
+}
