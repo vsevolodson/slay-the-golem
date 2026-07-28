4de27e65f39adecd9111a0a3583c5b35f0096125
vsevolodson <son.sev.06@gmail.com>
2026-07-28 16:43:40 +0900

    feat: добавлен примерный combatstate

---
+++ Assets/Game.Core/State/CombatState.cs
@@ -0,0 +1,61 @@
+using System;
+using Newtonsoft.Json;
+
+public sealed class CombatState
+{
+    [JsonProperty] public PlayerSide Player { get; private set; }
+    [JsonProperty] public EnemySide Enemy { get; private set; }
+
+    [JsonProperty] public int TurnNumber { get; private set; }
+    [JsonProperty] public Side ActiveSide { get; private set; }
+    [JsonProperty] public CombatOutcome Outcome { get; private set; }
+
+    [JsonProperty] public ulong Seed { get; private set; }
+
+    private CombatState(PlayerSide player, EnemySide enemy, ulong seed)
+    {
+        Player = player;
+        Enemy = enemy;
+        Seed = seed;
+        TurnNumber = 0;
+        ActiveSide = Side.Player;
+        Outcome = CombatOutcome.InProgress;
+    }
+
+    [JsonConstructor]
+    private CombatState()
+    {
+    }
+
+    public static CombatState Create(CombatSetup setup)
+    {
+        if (setup == null)
+            throw new ArgumentNullException(nameof(setup));
+
+        var player = new PlayerSide(
+            new Combatant(setup.PlayerMaxHealth, setup.PlayerHealth),
+            setup.StartingDeck);
+
+        var enemy = new EnemySide(
+            new Combatant(setup.EnemyMaxHealth, setup.EnemyMaxHealth),
+            setup.EnemyId);
+
+        return new CombatState(player, enemy, setup.Seed);
+    }
+
+    public Combatant GetCombatant(Side side)
+    {
+        switch (side)
+        {
+            case Side.Player: return Player.Combatant;
+            case Side.Enemy: return Enemy.Combatant;
+            default: throw new ArgumentOutOfRangeException(nameof(side));
+        }
+    }
+
+    internal void SetActiveSide(Side side) => ActiveSide = side;
+
+    internal void AdvanceTurnNumber() => TurnNumber++;
+
+    internal void SetOutcome(CombatOutcome outcome) => Outcome = outcome;
+}
