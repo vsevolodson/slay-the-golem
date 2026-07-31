3bfb211b97f76f0d506fadc110e5142c9a2f79bd
vsevolodson <son.sev.06@gmail.com>
2026-07-31 11:59:53 +0900

    feat: effectcontext

---
+++ Assets/Game.Core/Effects/EffectContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Game.Core.State;
+
+namespace Game.Core.Effects
+{
+    public sealed class EffectContext
+    {
+        internal EffectContext(CombatState state, Side actor)
+        {
+            State = state ?? throw new ArgumentNullException(nameof(state));
+            Actor = actor;
+            Opponent = actor == Side.Player ? Side.Enemy : Side.Player;
+        }
+
+        public CombatState State { get; }
+        public Side Actor { get; }
+        public Side Opponent { get; }
+
+        public Combatant ActorCombatant => State.GetCombatant(Actor);
+        public Combatant OpponentCombotant => State.GetCombatant(Opponent);
+    }
+}
