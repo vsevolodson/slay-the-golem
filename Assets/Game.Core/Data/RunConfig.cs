c9e4d6d37fbf63b4ce8b63f0d29d24ff736dd755
vsevolodson <son.sev.06@gmail.com>
2026-08-06 13:58:10 +0900

    feat: RunConfig

---
+++ Assets/Game.Core/Data/RunConfig.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Game.Core.State;
+
+namespace Game.Core.Data
+{
+    public sealed class RunConfig
+    {
+        private readonly List<string> _fights;
+        private readonly List<CardId> _rewardPool;
+        private readonly List<CardId> _startingDeck;
+
+        public CombatConfig Combat { get; }
+        public int PlayerMaxHealth { get; }
+        public int RewardChoices { get; }
+
+        public IReadOnlyList<string> Fights => _fights;
+        public IReadOnlyList<CardId> RewardPool => _rewardPool;
+        public IReadOnlyList<CardId> StartingDeck => _startingDeck;
+
+        public RunConfig(CombatConfig combat, IEnumerable<string> fights, IEnumerable<CardId> rewardPool,
+            IEnumerable<CardId> startingDeck, int playerMaxHealth, int rewardChoices)
+        {
+            Combat = combat ?? throw new ArgumentNullException(nameof(combat));
+
+            _fights = new List<string>(fights ?? throw new ArgumentNullException(nameof(fights)));
+            _rewardPool = new List<CardId>(rewardPool ?? throw new ArgumentNullException(nameof(rewardPool)));
+            _startingDeck = new List<CardId>(startingDeck ?? throw new ArgumentNullException(nameof(startingDeck)));
+
+            if (_fights.Count == 0)
+                throw new ArgumentException("a run must have at least one fight", nameof(fights));
+            if (playerMaxHealth <= 0)
+                throw new ArgumentOutOfRangeException(nameof(playerMaxHealth));
+            if (rewardChoices <= 0)
+                throw new ArgumentOutOfRangeException(nameof(rewardChoices));
+
+            PlayerMaxHealth = playerMaxHealth;
+            RewardChoices = rewardChoices;
+        }
+    }
+}
